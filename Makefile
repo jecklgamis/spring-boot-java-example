387001125614bfc77f911b9f54f4a4d2e50a5335
Jerrico Gamis <jecklgamis@gmail.com>
2017-12-05 06:40:24 +1100

    Support HTTP and HTTPs

--- Makefile
+++ Makefile
@@ -5,6 +5,6 @@ dist:
 image:
 	docker build -t springboot-java-example:latest .
 run:
-	docker run -p 8080:8080  -p 8081:8081 springboot-java-example:latest
+	docker run -p 8080:8080  -p 8443:8443 springboot-java-example:latest
 run-bash:
 	docker run -i -t springboot-java-example:latest /bin/bash
--- src/main/java/springboot/java/example/ExampleApp.java
+++ src/main/java/springboot/java/example/ExampleApp.java
@@ -1,12 +1,28 @@
 package springboot.java.example;
 
+import org.eclipse.jetty.server.NetworkTrafficServerConnector;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerFactory;
+import org.springframework.boot.context.embedded.jetty.JettyEmbeddedServletContainerFactory;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ExampleApp {
     public static void main(String args[]) {
+
         SpringApplication.run(ExampleApp.class, args);
+    }
+
+    @Bean
+    public EmbeddedServletContainerFactory servletContainer() {
+        JettyEmbeddedServletContainerFactory factory = new JettyEmbeddedServletContainerFactory();
+        factory.addServerCustomizers(server -> {
+            final NetworkTrafficServerConnector connector = new NetworkTrafficServerConnector(server);
+            connector.setPort(8080);
+            server.addConnector(connector);
+        });
+        return factory;
     }
 
 }
