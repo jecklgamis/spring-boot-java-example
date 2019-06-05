d08fb8054730d7b7c4e64408e3ff3f7e0b6667f4
Jerrico Gamis <jecklgamis@gmail.com>
2019-06-05 19:59:25 +1000

    Update root controller, use Java time

--- src/main/java/springboot/java/example/ObjectMapperBuilderCustomizer.java
+++ src/main/java/springboot/java/example/ObjectMapperBuilderCustomizer.java
@@ -1,15 +1,15 @@
 package springboot.java.example;
 
-import com.fasterxml.jackson.datatype.joda.JodaModule;
-
 import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
 import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 import org.springframework.stereotype.Component;
+
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 
 @Component
 public class ObjectMapperBuilderCustomizer implements Jackson2ObjectMapperBuilderCustomizer {
 
     public void customize(Jackson2ObjectMapperBuilder builder) {
-        builder.modules(new JodaModule());
+        builder.modules(new JavaTimeModule());
     }
 }
--- src/main/java/springboot/java/example/controller/RootController.java
+++ src/main/java/springboot/java/example/controller/RootController.java
@@ -1,29 +1,29 @@
 package springboot.java.example.controller;
 
-import com.google.common.collect.ImmutableMap;
-import io.micrometer.core.annotation.Timed;
-import org.joda.time.LocalDateTime;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.google.common.collect.ImmutableMap;
+
+import io.micrometer.core.annotation.Timed;
+import java.time.LocalDateTime;
 import java.util.Map;
 
 import static org.springframework.web.bind.annotation.RequestMethod.GET;
 
 @RestController
 public class RootController {
-
-    @Autowired
-    public RootController() {
-    }
+    @Value("${application.name}")
+    private String appName;
 
     @RequestMapping(path = {"/"}, method = {GET}, produces = {"application/json"})
     @Timed
-    public Map example() {
+    public Map root() {
         return ImmutableMap.builder()
-                .put("message", "Java rocks!")
-                .put("today", LocalDateTime.now().toString())
+                .put("name", appName)
+                .put("java.version", System.getProperty("java.version"))
+                .put("now", LocalDateTime.now())
                 .build();
     }
 }
